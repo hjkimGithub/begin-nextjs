a253f88214af0615c0ab0ba23d69e7ea17b60491
hjkimGithub <rlaguswo3408@gmail.com>
2022-02-10 10:31:43 +0900

    next.config.js, hide API_KEY

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const API_KEY = process.env.API_KEY;
+// const API_KEY = "8c21ad057d279f4f68f173646c241447";
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
